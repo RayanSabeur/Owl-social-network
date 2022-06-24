1deb9b63b764bec1a4ad81bf45ef79299032e36b
RayanSabeur <85495356+RayanSabeur@users.noreply.github.com>
2022-06-25 01:08:04 +0200

    Update UpdateProfil.js

--- client/src/components/Profil/UpdateProfil.js
+++ client/src/components/Profil/UpdateProfil.js
@@ -13,27 +13,29 @@ import axios from 'axios';
 
 const UpdateProfil = ({user}) => {
 
-     const [bio, setBio] = useState("")
+     const [bio, setBio] = useState("");
      const [updateForm, setUpdateForm] = useState(false);
      const dispatch = useDispatch();
      const [followingPopup, setFollowingPopup] = useState(false);
      const [followersPopup, setFollowersPopup] = useState(false);
-     const path = "."
-     const uid = useContext(UidContext)
+     const path = ".";
+     const uid = useContext(UidContext);
  const usersData = useSelector((state) => state.allusersReducer);
     const userData = useSelector((state) => state.userReducer);
-    let chemin = "/profile/"
+    let chemin = "/profile/";
+     
+     
+     
     const handleUpdate = () => {
-     dispatch(updateBio(userData._id, bio))
+     dispatch(updateBio(userData._id, bio));
      setUpdateForm(false);
 
     }
 
     const handleConversation = async (e) => {
 
-    e.preventDefault()
-
-     
+    e.preventDefault();
+         
       await axios({
         method: "post",
         url: `${process.env.REACT_APP_API_URL}api/conversations`,
@@ -45,46 +47,47 @@ const UpdateProfil = ({user}) => {
       }).then((res) => {
         console.log(res.data)
        
-      })
+      });
+      window.location = '/messenger';
 
-      window.location = '/messenger'
-
- }   
+ }; 
 
     return (
-        <div className="profil-container">
+    <div className="profil-container">
         <LeftNav />
         <h1> Profil de {user.pseudo}</h1>
-        <div className="update-container">
-          <div className="left-part">
+     <div className="update-container">
+         <div className="left-part">
             <h3>Photo de profil</h3>
             <img src={path + user.picture} alt="user-pic" className="img-profil"/>
-            {uid === user._id ? (   <UploadImg user={user}/> ): (<div>  <img  src="/img/icons/enveloppe.png" className='img-share'  onClick={handleConversation} alt="envoyer un message privé" /> </div>)}
-          {uid === user._id ? (""): (<div>  Envoyer un message privé </div>)}
+                 
+            {uid === user._id ? (   <UploadImg user={user}/> ) : 
+            (<div>  <img  src="/img/icons/enveloppe.png" className='img-share'  onClick={handleConversation} alt="envoyer un message privé" /> </div>
+             )}
+          {uid === user._id ? ("") :
+          (<div>  Envoyer un message privé </div>
+            )}
+                                     
           </div>
           <div className="right-part">
             <div className="bio-update">
               <h3>Bio</h3>
               {updateForm === false && (
-                <>
-                  
-
+                <>          
                   {uid === user._id ? (
                     <>
                      <p onClick={() => setUpdateForm(!updateForm)}>{userData.bio}</p> 
                    <button onClick={() => setUpdateForm(!updateForm)}>
                     Modifier bio
                   </button>
-                  </> ): (<p >{user.bio}</p>) }
-
-                
-                 
-            
-
-                 
+                  </> ):
+                    (<p >{user.bio}</p>) 
+                     }
                 
                 </>
+                     
               )}
+                    
               {updateForm &&  (  uid === user._id ? 
                  ( <>
                   <textarea
@@ -99,12 +102,15 @@ const UpdateProfil = ({user}) => {
             <h4>Membre depuis le : {dateParser(user.createdAt)}</h4>
             <h5 onClick={() => setFollowingPopup(true)}>
               Abonnements : {user.following ? user.following.length : ""}
+
             </h5>
             <h5 onClick={() => setFollowersPopup(true)}>
               Abonnés : {user.followers ? user.followers.length : ""}
+
             </h5>
           </div>
         </div>
+
 { followingPopup && (
 <div className='popup-profil-container'>  
 <div className='modal'>
@@ -114,6 +120,7 @@ const UpdateProfil = ({user}) => {
     </span>
 
     <ul>
+         
 {usersData.map((use) => {
 
 for (let i = 0; i < user.following.length; i++) {
@@ -127,27 +134,28 @@ for (let i = 0; i < user.following.length; i++) {
             <h4>{use.pseudo}</h4>
             <div className="follow-handler">
           <FollowHandler idToFollow={use._id} type={'suggestion'}/>
+               
             </div>
           </li>
         )
-
         }
     }
     
 })}
     </ul>
+  </div>
 </div>
-</div>)}
 
+)}
 
 { followersPopup && (
 <div className='popup-profil-container'>  
 <div className='modal'>
-    <h3>Abonnée</h3>
+  <h3>Abonnée</h3>
     <span className='cross' onClick={() => setFollowersPopup(false)}>&#10005;
-    
     </span>
     <ul>
+         
 {usersData.map((use) => {
 
 for (let i = 0; i < user.followers.length; i++) {
@@ -163,15 +171,18 @@ for (let i = 0; i < user.followers.length; i++) {
             <h1><FollowHandler idToFollow={use._id} type={'suggestion'}/></h1>
             </div>
           </li>
-        )
+           )
 
         }
     }
-})}
-    </ul>
+}
+)}
+      </ul>
+    </div>
 </div>
-</div>)}
-        </div>
+
+)} 
+     </div>
     );
 };
 
